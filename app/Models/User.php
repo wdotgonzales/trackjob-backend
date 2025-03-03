8bf19b0ec54d043997adf7964b33933ca648ead7
wdotgonzales <wdotgonzales@gmail.com>
2025-03-03 15:46:26 +0800

    establish one-to-one relationship between User and Verification_Code Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -11,6 +12,10 @@ class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    public function verification_code(): HasOne{
+        return $this->hasOne(Verification_Code::class);
+    }
 
     /**
      * The attributes that are mass assignable.
--- app/Models/Verification_Code.php
+++ app/Models/Verification_Code.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Verification_Code extends Model
 {
+    public function user() : BelongsTo{
+        return $this->belongsTo(User::class);
+    }
     //
+    
 }
