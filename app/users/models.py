ed3093b20a5f01ebfd795b0dcf30840fe2773b23
Wilz Gonzales <wilson.gonzales@blaqbox.tech>
2025-07-27 21:44:40 +0800

    feat: add user model

--- app/users/models.py
+++ app/users/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table = 'tbl_user' 
