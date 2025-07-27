1f1acf15b4a1838715b7b5403462e6c5c73b83a4
Wilz Gonzales <wilson.gonzales@blaqbox.tech>
2025-07-27 21:51:34 +0800

    fix: modify joined_at column to updated_at

--- app/users/models.py
+++ app/users/models.py
@@ -6,7 +6,8 @@ class User(models.Model):
     username = models.CharField(max_length=150, unique=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=128)
-    date_joined = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.username
