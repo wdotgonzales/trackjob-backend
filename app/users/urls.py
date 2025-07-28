418aef203091b2370dde05b1b72840d46fdc7193
Wilz Gonzales <wilson.gonzales@blaqbox.tech>
2025-07-29 07:50:48 +0800

    feat: add urls related to verification code feature from views.py

--- app/users/urls.py
+++ app/users/urls.py
@@ -9,4 +9,7 @@ urlpatterns = [
     path('register/', RegisterViewSet.as_view({'post': 'register'}), name='user-register'),
     path('login/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('decode-token/', DecodeTokenView.as_view(), name='decode_token'),
+    
+    path('send-verification-code/', RegisterViewSet.as_view({'post': 'send_verification_code'}), name='send-verification-code'),
+    path('verify-code/',  RegisterViewSet.as_view({'post': 'verify_code'}), name='verify-code')
 ]
