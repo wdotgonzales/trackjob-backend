0704bb1276d01a8198912b522c91aba607047098
Wilz Gonzales <wilson.gonzales@blaqbox.tech>
2025-07-30 09:38:07 +0800

    feat: add permission, pagination configuration

--- app/trackjob/settings.py
+++ app/trackjob/settings.py
@@ -147,6 +147,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,  # Adjust as needed
 }
 
 SIMPLE_JWT = {
