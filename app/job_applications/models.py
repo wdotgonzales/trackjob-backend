245bc70d120950908a735d8c6d79ad586e399556
Wilz Gonzales <wilson.gonzales@blaqbox.tech>
2025-07-30 09:42:47 +0800

    feat: add models

--- app/job_applications/models.py
+++ app/job_applications/models.py
@@ -1,3 +1,77 @@
 from django.db import models
+from users.models import User
 
 # Create your models here.
+
+class EmploymentType(models.Model):
+    id = models.AutoField(primary_key=True)
+    label = models.CharField(max_length=128)
+    description = models.CharField(max_length=128)
+    
+    def __str__(self):
+        return self.label
+
+    class Meta:
+        db_table = 'tbl_employment_type'
+
+class WorkArrangement(models.Model):
+    id = models.AutoField(primary_key=True)
+    label = models.CharField(max_length=128)
+    description = models.CharField(max_length=128)
+    
+    def __str__(self):
+        return self.label
+
+    class Meta:
+        db_table = 'tbl_work_arrangement'
+
+    
+class JobApplicationStatus(models.Model):
+    id = models.AutoField(primary_key=True)
+    label = models.CharField(max_length=128)
+    description = models.CharField(max_length=128)
+    
+    def __str__(self):
+        return self.label
+
+    class Meta:
+        db_table = 'tbl_job_application_status'
+
+class JobApplication(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='job_application')
+    position_title = models.CharField(max_length=128)
+    company_name = models.CharField(max_length=128)
+    employment_type = models.ForeignKey(EmploymentType, on_delete=models.SET_NULL, null=True, related_name='employment_type')
+    work_arrangement = models.ForeignKey(WorkArrangement, on_delete=models.SET_NULL, null=True, related_name='work_arrangement')
+    job_application_status = models.ForeignKey(JobApplicationStatus, on_delete=models.SET_NULL, null=True, related_name='job_application_status')
+    job_posting_link = models.CharField(max_length=255)
+    date_applied = models.DateField()
+    company_logo_url = models.CharField(max_length=255, blank=True, null=True)
+    job_location = models.CharField(max_length=255)
+    job_description = models.CharField(max_length=255, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"{self.position_title} at {self.company_name}"
+
+    class Meta:
+        db_table = 'tbl_job_application'
+
+
+class Reminder(models.Model):
+    id = models.AutoField(primary_key=True)
+    job_application_id = models.ForeignKey(JobApplication, on_delete=models.CASCADE, related_name='reminders')
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    isReminderUsed = models.BooleanField(default=False)
+    time_set = models.DateTimeField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"Reminder: {self.title}"
+
+    class Meta:
+        db_table = 'tbl_reminder'
